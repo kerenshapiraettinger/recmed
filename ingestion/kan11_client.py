7ba4e63f0eba72862555b8097a5dbc8ffca50d38
kerenshapiraettinger <kerenshapiraettinger@users.noreply.github.com>
2026-03-21 13:11:34 +0200

    Improve Kan 11 request headers to bypass bot detection

--- ingestion/kan11_client.py
+++ ingestion/kan11_client.py
@@ -12,7 +12,15 @@ HEADERS = {
         "Chrome/124.0.0.0 Safari/537.36"
     ),
     "Accept-Language": "he-IL,he;q=0.9,en;q=0.8",
-    "Accept": "text/html,application/xhtml+xml,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Cache-Control": "max-age=0",
 }
 
 
